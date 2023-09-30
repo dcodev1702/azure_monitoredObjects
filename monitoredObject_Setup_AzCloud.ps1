b1729b2b8399436e2902e1e5f229488161015356
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2023-09-30 15:07:53 -0400

    Update monitoredObject_Setup_AzCloud.ps1

--- monitoredObject_Setup_AzCloud.ps1
+++ monitoredObject_Setup_AzCloud.ps1
@@ -47,7 +47,17 @@ if ($index -ge 0 -and $index -lt $SetCloudEnv.Count) {
   Exit
 }
 
+#############################
+#0. Validate Data Collection Rule (Windows) Existence
+$requestURL = "$($resourceUrl)subscriptions/$SubscriptionID/resourceGroups/$ResourceGroup/providers/Microsoft.Insights/dataCollectionRules/$DCRName`?api-version=2022-06-01"
+$Respond = Invoke-RestMethod -Uri $requestURL -Headers $AuthenticationHeader -Method GET -Verbose
+if ($Respond -eq $null) {
+  Write-Host "Data Collection Rule $DCRName does not exist. Create DCR before proceeding, exiting script." -ForegroundColor Red
+  Exit
+}
+
 #Grant Access to User at root scope "/"
+#It is required that you have ownership level permissions at the TENANT LEVEL within Entra ID.
 $user = Get-AzADUser -UserPrincipalName (Get-AzContext).Account
 
 New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $user.Id
@@ -60,14 +70,7 @@ $AuthenticationHeader = @{
   "Authorization" = "Bearer " + $auth.Token
 }
 
-#0. Validate Data Collection Rule Existence
-$requestURL = "$($resourceUrl)subscriptions/$SubscriptionID/resourceGroups/$ResourceGroup/providers/Microsoft.Insights/dataCollectionRules/$DCRName`?api-version=2022-06-01"
-$Respond = Invoke-RestMethod -Uri $requestURL -Headers $AuthenticationHeader -Method GET -Verbose
-if ($Respond -eq $null) {
-  Write-Host "Data Collection Rule $DCRName does not exist. Create DCR before proceeding, exiting script." -ForegroundColor Red
-  Exit
-}
-
+###########################
 #1. Assign ‘Monitored Object Contributor’ Role to the operator
 $newguid = (New-Guid).Guid
 $UserObjectID = $user.Id
@@ -82,16 +85,14 @@ $body = @"
 "@
 
 $requestURL = "$($resourceUrl)providers/microsoft.insights/providers/microsoft.authorization/roleassignments/$newguid`?api-version=2021-04-01-preview"
-
 Invoke-RestMethod -Uri $requestURL -Headers $AuthenticationHeader -Method PUT -Body $body
 
 ##########################
 #2. Create Monitored Object
 
-# "location" property value under the "body" section should be the Azure region where the MO object would be stored. It should be the "same region" where you created the Data Collection Rule. This is the location of the region from where agent communications would happen.
+# "location" property value under the "body" section should be the Azure region where the MO object would be stored. 
+# It should be the "same region" where you created the Data Collection Rule.
 $requestURL = "$($resourceUrl)providers/Microsoft.Insights/monitoredObjects/$TenantID`?api-version=2021-09-01-preview"
-
-# TODO: INSERT A CHECK (API CALL) TO VALIDATE $DCRName EXISTS BEFORE PROCEEDING!
 $Location   = (Get-AzDataCollectionRule -Name $DCRName -ResourceGroupName $ResourceGroup -WarningAction SilentlyContinue).Location
 
 $body = @"
@@ -107,7 +108,7 @@ $RespondID = $($Respond.id).Substring(1)
 
 ##########################
 #3. Associate DCR to Monitored Object
-#See reference documentation https://learn.microsoft.com/en-us/rest/api/monitor/data-collection-rule-associations/create?tabs=HTTP
+# See reference documentation https://learn.microsoft.com/en-us/rest/api/monitor/data-collection-rule-associations/create?tabs=HTTP
 $associationName = "MOAssoc01" #You can define your custom associationname, must change the association name to a unique name, if you want to associate multiple DCR to monitored object
 
 $requestURL = "$($resourceUrl)$RespondId/providers/microsoft.insights/datacollectionruleassociations/$associationName`?api-version=2022-06-01"
